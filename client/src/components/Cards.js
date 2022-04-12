99055ed5fcfcb6343471bc457d231583da4f4fd6
sungmin-choi <namja306@naver.com>
2022-04-12 16:10:14 +0900

    Feat)카드 공유시 문제집에서 공유된 카드 필터링하기

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Card from './Card';
 import CardsContainner from '../styles/CardsStyle';
 
 function Cards({ cardsInfo, handlePostCard }) {
   const { me } = useSelector((state) => state.user);
-  console.log(cardsInfo);
+  const [loginCardsInfo, setLoginCardsInfo] = useState([]);
+  useEffect(() => {
+    if (me) {
+      const filterCards = cardsInfo.filter(
+        (card) => !me.SharedIdArr.includes(card.id),
+      );
+      setLoginCardsInfo(filterCards);
+    }
+  }, [me, me?.Shared.length]);
   const handleStudyBtn = () => {
     handlePostCard(true);
   };
+  console.log('loginCardsInfo:', loginCardsInfo);
   return (
     <CardsContainner>
       {me ? (
-        <button onClick={handleStudyBtn} className="studyBtn" type="button">
-          문제 추가하기
-        </button>
-      ) : null}
-      {cardsInfo.map((cardInfo) => (
-        <Card key={cardInfo.id} cardRole="main" cardInfo={cardInfo} />
-      ))}
+        <>
+          <button onClick={handleStudyBtn} className="studyBtn" type="button">
+            문제 추가하기
+          </button>
+          {loginCardsInfo.map((cardInfo) => (
+            <Card key={cardInfo.id} cardRole="main" cardInfo={cardInfo} />
+          ))}
+        </>
+      ) : (
+        <>
+          {cardsInfo.map((cardInfo) => (
+            <Card key={cardInfo.id} cardRole="main" cardInfo={cardInfo} />
+          ))}
+        </>
+      )}
     </CardsContainner>
   );
 }
