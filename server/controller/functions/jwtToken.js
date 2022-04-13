6cff9f443a5a26ac3c4134b20932a4043cb0bfe7
sungmin-choi <namja306@naver.com>
2022-04-13 23:33:40 +0900

    fix) accessToken 만료기간 연장

--- server/controller/functions/jwtToken.js
+++ server/controller/functions/jwtToken.js
@@ -4,20 +4,29 @@ const jwt = require('jsonwebtoken');
 module.exports = {
   // Access token으로 sign
   generateAccessToken: (data) => {
-    return jwt.sign(data, process.env.ACCESS_SECRET, { expiresIn: '1d' });
+    return jwt.sign(data, process.env.ACCESS_SECRET, {
+      expiresIn: '3h',
+      issuer: 'cotak',
+    });
+  },
+  generateRefreshToken: () => {
+    return jwt.sign({}, process.env.ACCESS_SECRET, {
+      expiresIn: '14d',
+      issuer: 'cotak',
+    });
   },
   // JWT 토큰을 쿠키로 전달
-  sendAccessToken: (res, accessToken) => {
+  sendToken: (res, accessToken, refreshToken) => {
     const cookieOptions = {
-      maxAge: 900000,
       httpOnly: true,
     };
     res.cookie('accessToken', accessToken, cookieOptions);
+    res.cookie('refreshToken', refreshToken, cookieOptions);
   },
   // JWT 토큰 정보를 받아서 검증
-  checkAccessToken: (accessToken) => {
+  checkToken: (token) => {
     try {
-      return jwt.verify(accessToken, process.env.ACCESS_SECRET);
+      return jwt.verify(token, process.env.ACCESS_SECRET);
     } catch (err) {
       return null;
     }
